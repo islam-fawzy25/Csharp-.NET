7d792142800a7685e9d94a8d0149675a15a13e17
Islam <islam.fawzy25@hotmail.com>
2022-05-16 20:15:19 +0200

    test

--- .NET/1-Models/OperasWebApp/OperasWebApp/Models/Opera.cs
+++ .NET/1-Models/OperasWebApp/OperasWebApp/Models/Opera.cs
@@ -10,14 +10,11 @@ namespace OperasWebApp.Models
     {
         [Key]
         public int OperaId { get; set; }
-
         [Required]
         [StringLength(100)]  // eller [MaxLength(100)]  
         public string Title { get; set; }
-
         [CheckValidYear]  // 1598
         public int Year { get; set; }
-
         [Required]
         public string Composer { get; set; }
     }
