f7da466db3af55806956d5c95c5b7a83eb23736c
Islam <islam.fawzy25@hotmail.com>
2022-05-16 20:04:32 +0200

    intial commit

--- .NET/1-Models/OperasWebApp/OperasWebApp/Models/Opera.cs
+++ .NET/1-Models/OperasWebApp/OperasWebApp/Models/Opera.cs
@@ -10,11 +10,14 @@ namespace OperasWebApp.Models
     {
         [Key]
         public int OperaId { get; set; }
+
         [Required]
         [StringLength(100)]  // eller [MaxLength(100)]  
         public string Title { get; set; }
+
         [CheckValidYear]  // 1598
         public int Year { get; set; }
+
         [Required]
         public string Composer { get; set; }
     }
